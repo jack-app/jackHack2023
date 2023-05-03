18cb622400befaf5f45e1bcfb90db1bcc0d2df61
HBenpitsu <hbenpitsu15@gmail.com>
2023-05-03 15:30:47 +0900

    APIKeyをロード

--- backend/image.py
+++ backend/image.py
@@ -1,9 +1,19 @@
 # ここに画像検索のAPIを叩く処理を書いてね
+import requests
+import json
+from os import chdir,getcwd
+from os.path import dirname
 
+if __name__ == "__main__":
+    chdir(dirname(__file__))
+    print(getcwd())
+
+with open("UnsplashAPIkey.json") as f:
+    KEYS = json.load(f)
+    
 def image(flower_name):
     print(flower_name)
-
-    # ここに画像検索のAPIを叩く処理を書いてね
-    # 以下に示した例みたいな形で、urlをreturnすることができたらok!
-
     return "https://picsum.photos/200/300"#例示を画像URLに変更
+
+if __name__ == "__main__":
+    print(KEYS)
