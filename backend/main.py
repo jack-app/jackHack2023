ff4371bdb5ac3ca9decad56c2f811a0c3dcfecba
yutteee <yusaku_nakamura_aa@icloud.com>
2023-05-03 14:00:35 +0900

    chat.pyの修正

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+import chat
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello Flask"
+    
+    content = {
+        "opponent": "お母さん",
+        "feeling": "感謝の気持ち"
+    }
+    aaa = chat.chat(content)
+
+    return aaa
 
 if __name__ == '__main__':
     app.run()
