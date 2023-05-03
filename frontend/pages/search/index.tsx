b38e0868f6e27d45c14ab6bc818209a27ab05f90
yutteee <yusaku_nakamura_aa@icloud.com>
2023-05-03 11:22:44 +0900

    フロントの環境構築

---
+++ frontend/pages/search/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <>
+      <div>
+        <h1 className="tile">検索するとこです。</h1>
+      </div>
+      <style jsx>{`
+        .title {
+          font-size: 50px;
+        }
+      `}</style>
+    </>
+  );
+}
