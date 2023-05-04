00b040d81fa6076dc6604e4a7997f6f60e915218
yutteee <yusaku_nakamura_aa@icloud.com>
2023-05-04 20:06:18 +0900

    corsの修正

--- backend/main.py
+++ backend/main.py
@@ -4,31 +4,30 @@ import chat
 import image
 
 app = Flask(__name__)
-cors = CORS(app, resouse = {r"/localhost": {"origins": "3000"}})
+cors = CORS(app, resources={r"/*": {"origins": "http://localhost:3000"}})
 
 @app.route('/', methods=['POST'])
 def main():
     if request.method == 'POST':
-      
-      req = request.get_json()
-      content = req["content"]
-      print(content)
 
-      # こきくんがやってくれる、chatGPTの関数に値を渡す処理
-      flower_info = chat.chat(content)
-      if(type(flower_info) != dict):
-          return "回答生成に失敗しました", 500
+        req = request.get_json()
+        content = req["content"]
+        print(content)
 
-      # えんぴつくんがやってくれる、画像検索から画像のurlを取得する処理
-      image_url = image.image(flower_info["name"])
+        # こきくんがやってくれる、chatGPTの関数に値を渡す処理
+        flower_info = chat.chat(content)
+        if(type(flower_info) != dict):
+            return "回答生成に失敗しました", 500
 
-      # エラー処理
-      if(image_url == 0):
-          return "画像検索に失敗しました", 500
+        # えんぴつくんがやってくれる、画像検索から画像のurlを取得する処理
+        image_url = image.image(flower_info["name"])
 
-      
-      flower_info["image"] = image_url
-      return flower_info
+        # エラー処理
+        if(image_url == 0):
+            return "画像検索に失敗しました", 500
+
+        flower_info["image"] = image_url
+        return flower_info
 
 if __name__ == '__main__':
     app.run()
