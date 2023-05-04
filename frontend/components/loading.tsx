2fc62bbc335f6718c4b92a676e2620782291d450
yutteee <yusaku_nakamura_aa@icloud.com>
2023-05-04 20:23:08 +0900

    ローディング画面を作成

--- frontend/components/loading.tsx
+++ frontend/components/loading.tsx
@@ -5,9 +5,20 @@ export const Loading: React.FC = () => {
     <>
       <div className="loading">
         <div className="circle"></div>
+        <div className="text">お花を摘んでいます...</div>
       </div>
 
       <style jsx>{`
+        .text {
+          position: absolute;
+          padding-top: 160px;
+          top: calc(50%);
+          left: calc(50%);
+          transform: translate(-50%, -50%);
+          font-size: 1.5rem;
+          font-family: serif;
+          font-weight: 100;
+        }
         .loading {
           opacity: 0.6;
           position: absolute;
@@ -18,18 +29,16 @@ export const Loading: React.FC = () => {
         }
 
         .circle {
-          width: 100px;
-          height: 100px;
+          width: 60px;
+          height: 60px;
           border-radius: 150px;
-          border: 10px solid #fff;
+          border: 5px solid #fff;
           border-top-color: rgba(0, 0, 0, 0.2);
           box-sizing: border-box;
           position: absolute;
-          top: 50%;
-          left: 50%;
-          transform: translate(-50%, -50%);
+          top: calc(50% - 30px);
+          left: calc(50% - 30px);
           animation: circle 1s linear infinite;
-          -webkit-animation: circle 1s linear infinite;
         }
         @keyframes circle {
           0% {
