1c9cca8de1f67fee7e1328afe8c2113c8b7f6609
HBenpitsu <hbenpitsu15@gmail.com>
2023-05-04 10:36:16 +0900

    使用APIの変更を可能にする・APIKeyの隠匿方法を更新する

--- backend/image.py
+++ backend/image.py
@@ -9,7 +9,7 @@ from datetime import datetime
 #定数の設定
 HOWMANY = 1
 ASPECTRATE = "4:3" #横:縦
-
+API = "unsplash"#unsplash/
 #Jsonの読み込み
 if __name__ == "__main__":
     chdir(dirname(__file__))
@@ -17,11 +17,14 @@ if __name__ == "__main__":
 with open("UnsplashAPIkey.json") as f:
     KEYS = load(f)
 
+
+#画像検索APIにunsplashを用いる場合
+
 #サーバーの起動中保持する変数を設定
 last_update = datetime.now().hour
 request_is_vailed = True
 
-def image(flower_name):
+def unsplash(flower_name):
     """花の写真を指すurlを返します。
     flower_nameは必ず英名の文字列を渡してください。
     何らかのエラーにより画像を取得できなかった場合はintの0を返します。
@@ -39,8 +42,8 @@ def image(flower_name):
     #unsplashAPIに{flower_name}の写真を1つ要求
     try:
         result = get("https://api.unsplash.com/photos/random/?"
-                 +f"client_id={KEYS['AccessKey']}"
-                 +f"&query={flower_name}"
+                 +f"client_id={KEYS['Unsplash']['AccessKey']}"
+                 +f"&query=\"{flower_name}\""
                  +f"&count={HOWMANY}")
     except:
         return 0
@@ -59,9 +62,17 @@ def image(flower_name):
 
     #APIの機能を用いて画像を成形
     #refer "https://unsplash.com/documentation#supported-parameters"
-    image_url = raw_image_url + f"&ar=4:3&crop=entropy&fit=crop"
+    image_url = raw_image_url + f"&ar={ASPECTRATE}&crop=entropy&fit=crop"
 
     return image_url
 
+
+
+
+def image(flower_name):
+    if API=="unsplash":
+        return unsplash(flower_name)
+
+
 if __name__ == "__main__":
-    print(image("Iris"))
+    print(image("flower blue rose"))
