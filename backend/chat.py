622bf6ff0f96838adac2d50b79d0ee17a06c6eee
“kidanikouki” <kidanikouki@gmail.com>
2023-05-04 14:17:52 +0900

    chat.pyの修正

--- backend/chat.py
+++ backend/chat.py
@@ -6,7 +6,7 @@ sys.stderr = io.TextIOWrapper(sys.stderr.buffer, encoding='utf-8')
 import openai
 
 # OpenAI APIを設定
-openai.api_key = "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"
+openai.api_key = "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"
 model_engine = "text-davinci-002"
 
 
@@ -25,8 +25,15 @@ def chat(content):
     )
 
     responsefix = bytes(response["choices"][0]["message"]["content"], encoding="utf-8").decode("utf-8")
-    print(responsefix)
-    print(type(responsefix))
+    responsefix1 = list(map(str,responsefix.split()))
+    print(responsefix1)
+    result = {
+        "name" : responsefix1[0],
+        "flower_symbolism" : responsefix1[1],
+        "example" : responsefix1[2]
+    }
+    print(result)
+    print(type(result))
     # result = {
     #     "name": name,
     #     "flower_symbolism": flower_symbolism,
